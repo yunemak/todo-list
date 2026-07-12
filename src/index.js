99b6775bb23d25086a29059364579c2c83b6cde9
yunemak <yeak558@gmail.com>
2026-07-12 15:55:03 +0300

    feat: Add createTask functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 import "./style.css";
 
-console.log("Hello worldaa!");
+const taskList = document.querySelector(".task-list");
+
+
+let task1 = createTask("den", "a", "23", "low");
+renderTask(task1);
+
+
+
+// functions
+
+function createTask(title, description, dueDate, priority) {
+	return {
+		title,
+		description,
+		dueDate,
+		priority,
+	}
+};
+
+function renderTask(task) {
+	let div = document.createElement("div");
+	div.classList.add("task");
+
+	div.appendChild(createTitle(task.title));
+
+	taskList.appendChild(div);
+}
+
+function createTitle(title) {
+	let h3 = document.createElement("h3");
+	h3.textContent = title;
+	return h3;
+}
+
+function createDescription(description) {
+
+}
+
+function createDueDate(dueDate) {
+
+}
+
+function createPriority(priority) {
+
+}
