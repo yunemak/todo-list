d0586e5b7b04bfac53b0a06f385f38c7c763a760
yunemak <yeak558@gmail.com>
2026-07-20 09:40:01 +0300

    feat: Create workspace

--- src/sidebar.js
+++ src/sidebar.js
@@ -2,6 +2,12 @@ const addWorkspaceBtn = document.getElementById("add-workspace-btn");
 const closeSidebarBtn = document.getElementById("close-sidebar-btn");
 const openSidebarBtn = document.getElementById("open-sidebar-btn");
 const sidebar = document.querySelector(".sidebar");
+const workspacesDiv = document.querySelector(".workspaces");
+
+let initialWorkspace = createWorkspace("My Workspace");
+let workspaces = [initialWorkspace];
+
+renderWorkspaces();
 
 closeSidebarBtn.addEventListener("click", (e) => {
 	sidebar.style.display = "none";
@@ -12,3 +18,23 @@ openSidebarBtn.addEventListener("click", (e) => {
 	sidebar.style.display = "flex";
 	openSidebarBtn.style.display = "none";
 });
+
+addWorkspaceBtn.addEventListener("click", (e) => {
+	let workspace = createWorkspace("deneme");
+	workspaces.push(workspace);
+	renderWorkspaces();
+});
+
+function createWorkspace(name) {
+	let taskList = [];
+	return { name, taskList };
+};
+
+function renderWorkspaces() {
+	workspacesDiv.replaceChildren();
+	workspaces.forEach((workspace) => {
+		let div = document.createElement("div");
+		div.textContent = workspace.name;
+		workspacesDiv.appendChild(div);
+	});
+};
