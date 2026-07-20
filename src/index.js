d399ddc8f5359c8c021ac96c7c5070f6ef12b425
yunemak <yeak558@gmail.com>
2026-07-20 09:20:53 +0300

    feat: Close/Open sidebar

--- src/index.js
+++ src/index.js
@@ -5,12 +5,23 @@ import { taskManager } from "./taskManager.js";
 let task1 = createTask("den", "a", "23", "low");
 addTask(task1);
 
-const addWorkspaceBtn = document.getElementById("add-workspace-btn");
-
-
-
 addTaskBtn.addEventListener("click", () => {
 	mainContainer.style.filter = "blur(10px)";
 	taskManager.style.display = "flex";
 	addTaskBtn.style.cursor = "default";
+});
+
+const addWorkspaceBtn = document.getElementById("add-workspace-btn");
+const closeSidebarBtn = document.getElementById("close-sidebar-btn");
+const openSidebarBtn = document.getElementById("open-sidebar-btn");
+const sidebar = document.querySelector(".sidebar");
+
+closeSidebarBtn.addEventListener("click",(e) => {
+	sidebar.style.display = "none";
+	openSidebarBtn.style.display = "inline";
+});
+
+openSidebarBtn.addEventListener("click", (e) => {
+	sidebar.style.display = "flex";
+	openSidebarBtn.style.display = "none";
 });
