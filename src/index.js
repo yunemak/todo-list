c40c50759e8ed68fdfe6302d365b134cbc11939e
yunemak <yeak558@gmail.com>
2026-07-20 09:02:35 +0300

    Add 'add workspace btn' , start toggle sidebar btn

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import { taskManager } from "./taskManager.js";
 let task1 = createTask("den", "a", "23", "low");
 addTask(task1);
 
+const addWorkspaceBtn = document.getElementById("add-workspace-btn");
+
+
+
 addTaskBtn.addEventListener("click", () => {
 	mainContainer.style.filter = "blur(10px)";
 	taskManager.style.display = "flex";
