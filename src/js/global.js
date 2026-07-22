dce0afd9b6be88b2bffb162ebfca187f5539a360
yunemak <yeak558@gmail.com>
2026-07-22 12:12:29 +0300

    feat: Add checkbox, duedate, priority to task

--- src/js/global.js
+++ src/js/global.js
@@ -14,7 +14,27 @@ function createTask(title, description, dueDate, priority) {
 function addTask(task) {
 	let div = document.createElement("div");
 	div.classList.add("task");
-	div.textContent = task.title;
+
+	let checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
+	div.appendChild(checkbox);
+	
+	let priority = document.createElement("p");
+	// Make color later
+	priority.textContent = task.priority;
+	div.appendChild(priority);
+
+
+	let h3 = document.createElement("h3");
+	h3.textContent = task.title;
+	div.appendChild(h3);
+
+	
+	let dueDate = document.createElement("div");
+	dueDate.textContent = task.dueDate;
+	div.appendChild(dueDate);
+
+
 	taskList.appendChild(div);
 }
 
