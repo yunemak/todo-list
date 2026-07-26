0826e74b9e20ae142390b0b0fb6e5afeff26630c
yunemak <yeak558@gmail.com>
2026-07-26 11:49:32 +0300

    feat: Add another workspace

--- src/js/workspaceManager.js
+++ src/js/workspaceManager.js
@@ -3,8 +3,11 @@ import { mainContainer } from "./global.js";
 const workspaceManager = document.getElementById("workspace-manager");
 const addWorkspaceBtn = document.getElementById("add-workspace-btn");
 const workspacesDiv = document.querySelector(".workspaces");
-const confirmWorkspaceBtn = document.getElementById("confirm-workspace-btn");
+const workspaceForm = document.getElementById("workspace-form");
 const closeWorkspaceManagerBtn = document.getElementById("close-workspace-manager-btn");
+
+// Input
+const workspaceNameInput = document.getElementById("workspace-name");
 
 let initialWorkspace = createWorkspace("My Workspace");
 let workspaces = [initialWorkspace];
@@ -19,6 +22,14 @@ closeWorkspaceManagerBtn.addEventListener("click", (e) => {
 	closeWorkspaceManager();
 });
 
+workspaceForm.addEventListener("submit", (e) => {
+	console.log("here");
+	e.preventDefault();
+	workspaces.push(createWorkspace(workspaceNameInput.value));
+	renderWorkspaces();
+	console.log(workspaces);
+	closeWorkspaceManager();
+})
 
 function createWorkspace(name) {
 	let taskList = [];
