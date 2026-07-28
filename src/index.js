3375c2d638b95e19b67c0d62efb51b4a3f0811d9
yunemak <yeak558@gmail.com>
2026-07-28 20:34:03 +0300

    Start over with button -> create everything from scratch

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-import "./js/taskManager.js";
-import "./js/sidebar.js";
-import "./js/workspaceManager.js";
-import "./js/renderTask.js";
-import "./js/renderWorkspace.js";
-import { renderWorkspaces } from "./js/renderWorkspace.js";
-
-renderWorkspaces();
