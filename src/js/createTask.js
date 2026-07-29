ce3401326377b56ecdb2797195f1f706ff966076
yunemak <yeak558@gmail.com>
2026-07-29 14:08:15 +0300

    Add createTask

---
+++ src/js/createTask.js
@@ -0,0 +1,12 @@
+function createTask(title, description, dueDate, priority) {
+	return { title, description, dueDate, priority };
+}
+/* 
+Priority
+1 - low
+2 - medium
+3 - high
+
+*/
+
+export { createTask };
