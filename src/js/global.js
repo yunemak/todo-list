5b8fba30bc646210ff6206560b1fd360273a16e8
yunemak <yeak558@gmail.com>
2026-07-31 10:43:54 +0300

    feat: Render tasks according to different projects

--- src/js/global.js
+++ src/js/global.js
@@ -1,5 +1,6 @@
 const sideBar = document.getElementById("side-bar");
 const mainContainer = document.getElementById("main-container");
 const body = document.querySelector("body");
+const project = document.getElementById("project");
 
-export { sideBar, mainContainer, body };
+export { sideBar, mainContainer, body, project };
--- src/js/renderTasks.js
+++ src/js/renderTasks.js
@@ -1,5 +1,26 @@
+import { project } from "./global.js";
+
 function renderTasks(tasks) {
+	removeCurrentContainer();
+	const container = document.createElement("div");
+	container.id = "current-container";
+
 	console.log(tasks);
+	tasks.forEach((task) => {
+		const div = document.createElement("div");
+		container.appendChild(div);
+		div.classList.add("task");
+		div.textContent = task.title;
+	});
+
+	project.appendChild(container);
+}
+
+function removeCurrentContainer() {
+	const currentContainer = document.getElementById("current-container");
+	if (currentContainer !== null) {
+		currentContainer.remove();
+	}
 }
 
 export { renderTasks };
