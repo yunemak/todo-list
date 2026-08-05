9b3b4f527cdfddf9155762213910b90a5134846b
yunemak <yeak558@gmail.com>
2026-08-05 17:48:14 +0300

    Add title input to task manager

--- src/js/taskManager.js
+++ src/js/taskManager.js
@@ -10,7 +10,26 @@ function createTaskManager() {
 	const taskManager = document.createElement("div");
 	taskManager.classList.add("task-manager");
 	body.appendChild(taskManager);
+
+	taskManager.appendChild(createTitleInput());
+
 	return taskManager;
+}
+
+function createTitleInput() {
+	const titleContainer = document.createElement("div");
+	titleContainer.classList.add("title-container");
+
+	const titleLabel = document.createElement("label");
+	titleLabel.classList.add("title-label");
+	titleContainer.appendChild(titleLabel);
+	titleLabel.textContent = "Title:";
+
+	const titleInput = document.createElement("input");
+	titleInput.classList.add("title-input");
+	titleContainer.appendChild(titleInput);
+
+	return titleContainer;
 }
 
 function destroyTaskManager(taskManager) {
