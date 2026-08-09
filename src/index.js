c119a5f067b422d0f1ca5261cf1ec6ea1e36710e
yunemak <yeak558@gmail.com>
2026-08-09 12:38:26 +0300

    Separate project manager code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import "./style.css";
 import "./js/createTask.js";
 import "./js/renderProjects.js";
 import "./js/renderTasks.js";
-import "./js/projectManager.js";
 import "./js/taskManager.js";
 import "./js/projectUtils.js";
+import "./js/projectManager.js";
---
+++ src/js/createProjectManager.js
@@ -0,0 +1,60 @@
+import { mainContainer, body, addProjectButton } from "./global.js";
+import { createProject } from "./projectUtils.js";
+import { renderProjects } from "./renderProjects.js";
+import {
+	createCloseButton,
+	setBackgroundDisabled,
+	createConfirmButton,
+} from "./managerCommon.js";
+
+function createProjectManager() {
+	const projectManager = document.createElement("div");
+	projectManager.classList.add("project-manager");
+	body.appendChild(projectManager);
+
+	projectManager.appendChild(createCloseButton(projectManager));
+
+	projectManager.appendChild(createFormElement(projectManager));
+}
+
+function createFormElement(projectManager) {
+	const formElement = document.createElement("form");
+	formElement.appendChild(createNameInput());
+	formElement.appendChild(createConfirmButton(projectManager));
+
+	formElement.addEventListener("submit", (e) => {
+		e.preventDefault();
+		const nameInput = document.getElementById("name-input");
+		createProject(nameInput.value);
+		renderProjects();
+		destroyProjectManager(projectManager);
+	});
+
+	return formElement;
+}
+
+function createNameInput() {
+	const inputContainer = document.createElement("div");
+	inputContainer.classList.add("input-container");
+
+	const nameLabel = document.createElement("label");
+	inputContainer.appendChild(nameLabel);
+	nameLabel.textContent = "Name:";
+	nameLabel.setAttribute("for", "name-input");
+
+	const nameInput = document.createElement("input");
+	inputContainer.appendChild(nameInput);
+	nameInput.classList.add("name-input");
+	nameInput.setAttribute("id", "name-input");
+	nameInput.setAttribute("minlength", "1");
+	nameInput.setAttribute("required", "");
+
+	return inputContainer;
+}
+
+function destroyProjectManager(projectManager) {
+	projectManager.remove();
+	setBackgroundDisabled(false);
+}
+
+export { createProjectManager };
