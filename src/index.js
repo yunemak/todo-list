997dd5c629cb8a985bd3aac9aa274dffd15c15be
yunemak <yeak558@gmail.com>
2026-08-10 14:50:25 +0300

    feat: Initially a default project is opened

--- src/index.js
+++ src/index.js
@@ -6,5 +6,28 @@ import "./js/projectUtils.js";
 import "./js/projectManagerEvents.js";
 import "./js/taskManagerEvents.js";
 import { renderProjects } from "./js/renderProjects.js";
+import { createProject } from "./js/projectUtils.js";
+import { createTask } from "./js/taskUtils.js";
+import { renderTasks } from "./js/renderTasks.js";
+import {
+	getLocalProjects,
+	updateLocalStorage,
+} from "./js/localStorageUtils.js";
 
 renderProjects();
+
+if (!localStorage.getItem("projects")) {
+	let projects = getLocalProjects();
+
+	let defaultProject = createProject("Default");
+	defaultProject.taskList.push(
+		createTask("Your very first task", "description", "2026-08-08"),
+	);
+
+	projects.push(defaultProject);
+
+	updateLocalStorage(projects);
+
+	renderProjects();
+	renderTasks(defaultProject);
+}
